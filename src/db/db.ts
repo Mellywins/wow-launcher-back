c0858faf51833e0297bf4c005c75f43dc264feea
Mellywins <zouaghi.wow1919@gmail.com>
2020-07-29 23:37:25 +0100

    code clean up v2

--- src/db/db.ts
+++ src/db/db.ts
@@ -11,17 +11,5 @@ const db = {
         return pool.query(text, params, callback)
     }
 }
-// const client = new Client({
-//     user: 'melly',
-//     host: 'localhost',
-//     database: 'wow',
-//     password: '1999',
-//     port: 5400
-// })
-// client.connect()
-//     .then(() => console.log('connected sucessfully!'))
-//     .then(() => client.query('select * from account'))
-//     .then((results: any) => console.table(results.rows))
-//     .catch((err: any) => console.error(err))
-//     .finally(() => client.end())
+
 export default db;
